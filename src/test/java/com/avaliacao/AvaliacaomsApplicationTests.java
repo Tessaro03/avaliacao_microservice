32bb86f92d338db05bec403375a710503ea88bcb
Gabriel Tessaro Favareto <gabrieltessarofavareto@gmail.com>
2024-06-01 15:52:51 -0300

    configuração para o Docker

--- src/test/java/com/avaliacao/AvaliacaomsApplicationTests.java
+++ src/test/java/com/avaliacao/AvaliacaomsApplicationTests.java
@@ -3,7 +3,7 @@ package com.avaliacao;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AvaliacaomsApplicationTests.class)
 class AvaliacaomsApplicationTests {
 
 	@Test
