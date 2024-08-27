7659f4e4641f5aa7b7297299ff161f43e7279d17
Gabriel Tessaro Favareto <gabrieltessarofavareto@gmail.com>
2024-08-27 15:09:55 -0300

    Adicionado validação para nota max e min

--- src/main/java/com/avaliacao/dtos/avaliacao/AvaliacaoInputDTO.java
+++ src/main/java/com/avaliacao/dtos/avaliacao/AvaliacaoInputDTO.java
@@ -1,9 +1,19 @@
 package com.avaliacao.dtos.avaliacao;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public record AvaliacaoInputDTO(
 
+    @NotNull
     Long idAvaliacao,
+
+    @NotNull
+    @Min(0)
+    @Max(5)
     Double nota,
+    
     String observacao
 
 ) {
